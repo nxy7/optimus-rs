771b48fc8c00f0ed8b9ce538201b2396dbe254a2
nxyt <lolnoxy@gmail.com>
2023-08-05 22:32:09 +0200

    cargo init

--- flake.nix
+++ flake.nix
@@ -1,17 +1,42 @@
 {
-  description = "Project starter";
+  description = "Optimus";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakeUtils.url = "github:numtide/flake-utils";
     nix2container.url = "github:nlewo/nix2container";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flakeUtils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flakeUtils, nix2container, ... }@inputs:
+  outputs =
+    { self, nixpkgs, flakeUtils, nix2container, rust-overlay, ... }@inputs:
     flakeUtils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
           config.allowUnfree = true;
         };
-      in { devShell = pkgs.mkShell { packages = with pkgs; [ hello ]; }; });
+
+        languagePackages = with pkgs; [
+          rust-bin.beta.latest.default
+          rust-analyzer
+          clippy
+          cargo-watch
+          cargo-nextest
+          nushell
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = languagePackages;
+          # RUST_SRC_PATH =
+          #   "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
 }
+
